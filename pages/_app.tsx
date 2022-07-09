acc5a52951a7f2451b8efe0aeca2d44b0dac3810
cuzknothz <cuzknothz@gmail.com>
2022-07-09 22:19:10 +0700

    _

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,11 @@
-// import "@@/assets/css/main.scss";
-
-import '@@/assets/css/animate.css'
-import '@@/assets/css/bootstrap.min.css'
-import '@@/assets/css/hover-min.css'
-import '@@/assets/css/main.css'
-import '@@/assets/css/owl.carousel.min.css'
-import '@@/assets/fonts/elegantIcon/elegantIcon.css'
-import '@@/assets/fonts/font-awesome/css/font-awesome.min.css'
-import '@@/assets/fonts/icofont/icofont.min.css'
-import '@@/assets/fonts/WebFont/style.css'
+import '@@/assets/css/index.scss'
+import { store } from '@@/store'
 
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ReactElement, ReactNode } from 'react'
+import { Provider } from 'react-redux'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -30,7 +22,9 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         <title>AG-Ecome | Thế giới mua sắm trực tuyến</title>
         <link rel="icon" href="favicon.ico" type="images/png" />
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   )
 }
